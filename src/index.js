b830a664695fb28a0b6f0388d1b2fbc058c1ba4a
Diego Diaz De La Concha <diegodiazdelaconcha93@gmail.com>
2024-01-28 23:22:37 -0600

    menu hamburguesa avance

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+
+const burguerIcon = document.querySelector('.burguer-icon');
+const menu = document.querySelector('.burguer-menu');
+
+burguerIcon.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('inactive');
+    console.log('clicked');
+}
+
